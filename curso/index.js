0f1487babb4fb32cb5261aca64a3fa5ea0ed372a
Carlos Henrique <krloss@github.com>
2017-12-02 16:59:06 -0200

    timers

--- curso/index.js
+++ curso/index.js
@@ -6,3 +6,20 @@ var moduloCache = require('./modulos/modulo.js')
 console.log(modulo);
 console.log(modulo === moduloCache);
 console.log(modulo.geraSerial());
+
+var i = setInterval(function () {
+	console.time('x');
+	console.log('a ' + new Date());
+
+	setTimeout(function () {
+		console.log('b ' + new Date());
+		clearInterval(i);
+	},3000);
+
+	setImmediate(function () {
+		console.log('c ' + new Date());
+	});
+
+	console.log('d ' + new Date());
+	console.timeEnd('x');
+},1000);
