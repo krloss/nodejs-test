e42244d2faf7d418e3fbe936bdc34314558c14d4
Carlos Henrique <krloss@github.com>
2017-12-01 22:12:14 -0200

    modulo cache

--- curso/index.js
+++ curso/index.js
@@ -1,6 +1,8 @@
 global.max = 100;
 
 var modulo = require('./modulos/modulo.js')
+var moduloCache = require('./modulos/modulo.js')
 
 console.log(modulo);
+console.log(modulo === moduloCache);
 console.log(modulo.geraSerial());
